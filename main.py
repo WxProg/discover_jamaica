976dac63d355652fd401e124d04038198d10ef62
mysta <chadmysta18@gmail.com>
2022-12-30 12:55:06 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import turtle
+
+# TODO: Setting up screen
+screen = turtle.Screen()
+screen.setup(1040, 689)
+screen.title("DISCOVER JAMAICA")
+image = "jamaica_blank_map.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+
+
+
+
+# keeps the turtle screen running
+turtle.mainloop()
